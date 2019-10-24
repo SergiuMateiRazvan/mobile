39932791f06092b71e6eece2761c5ab591781efd
Sergiu <mateisergiu777@gmail.com>
2019-10-24 11:18:36 +0300

    Lab1

--- App/App.js
+++ App/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Text, StyleSheet} from 'react-native';
 
 export default function App() {
-  return <Text>Hello</Text>;
+  return <Text style={styles.middle}>Hello, World!</Text>;
 }
+
+const styles = StyleSheet.create({
+  middle: {
+    textAlign: 'center',
+  },
+});
