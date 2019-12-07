3b65034cc7f3812baa97c0bb96b9f65b920e10d1
Sergiu <mateisergiu777@gmail.com>
2019-12-07 18:17:28 +0200

    Lab5

--- AppAndroid/app/src/main/java/app/app/MainActivity.kt
+++ AppAndroid/app/src/main/java/app/app/MainActivity.kt
@@ -1,6 +1,18 @@
 package app.app
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.ContentValues
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
@@ -11,6 +23,8 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.task_list_fragment.*
+
 
 val Any.TAG: String
     get() {
@@ -20,17 +34,53 @@ val Any.TAG: String
 
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
+    var image_uri: Uri? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        capture_btn.setOnClickListener{
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (checkSelfPermission(Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_DENIED ||
+                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_DENIED){
+                    val permission = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    requestPermissions(permission, 1000)
+                }
+                else{
+                    openCamera()
+                }
+            }
+            else{
+                openCamera()
+            }
+        }
         appBarConfiguration =
             AppBarConfiguration.Builder(R.id.task_list_fragment, R.id.task_edit_fragment)
                 .build()
         val navController: NavController = findNavController(R.id.nav_host_fragment)
         setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    private fun openCamera(){
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "New Picture")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
+        image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
+        startActivityForResult(cameraIntent, 1001)
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK){
+            image_view.setImageURI(image_uri)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
